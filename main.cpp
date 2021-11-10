34a92892b8cdd7c35aea4b08639690034b116c3a
AFuselli <91535784+AFuselli@users.noreply.github.com>
2021-11-10 21:04:41 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,5 +2,18 @@
 using namespace std;
 
 int main() {
-   return 0;
+    int n=0 , x=1 ,y=1 , z=0;
+    cin >> n;
+    cout << x << endl;
+    if(n>=2){
+        for(int i=0;i<n-1;i++){
+            x=y+z;
+            z=y;
+            y=x;
+            cout << x << endl;
+        }
+    }else{
+        cout << "errore";
+    }
+    return 0;
 }
